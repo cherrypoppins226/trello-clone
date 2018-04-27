87f0ca4cef68ca50e7c221a1ccb2e82ddc2062eb
Alejandro Hernandez <alejandro@peggedsoftware.com>
2018-04-27 17:39:12 -0400

    Improve visual design of lists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { withStyles } from "material-ui/styles";
 import Grid from "material-ui/Grid";
 import Paper from "material-ui/Paper";
 import List, { ListItem, ListItemText } from "material-ui/List";
@@ -7,45 +8,82 @@ import red from "material-ui/colors/red";
 
 import "./App.css";
 
-const CardListItem = props => {
+const CardListItem = withStyles({
+  paper: {
+    margin: 7,
+    background: red[100]
+  },
+  listItem: {
+    padding: 4,
+    paddingLeft: 8
+  }
+})(props => {
+  const { classes } = props;
   return (
-    <Paper style={{ background: red[100] }}>
-      <ListItem button={true}>
-        <ListItemText>{props.text}</ListItemText>
+    <Paper elevation={1} className={classes.paper}>
+      <ListItem button={true} className={classes.listItem}>
+        <ListItemText className={classes.listItemText}>
+          {props.text}
+        </ListItemText>
       </ListItem>
     </Paper>
   );
-};
+});
 
-const CardList = props => {
+const CardList = withStyles({
+  container: {
+    background: red[50]
+  },
+  header: {
+    padding: 0,
+    margin: 8,
+    fontWeight: 700
+  },
+  list: {
+    padding: 1
+  },
+  cards: {
+    maxHeight: "80vh",
+    overflowY: "scroll"
+  }
+})(props => {
+  const { classes } = props;
   return (
-    <Paper style={{ background: red[50] }}>
-      <List subheader={<ListSubheader>{props.name}</ListSubheader>}>
-        <Grid container spacing={16} direction="column">
+    <Paper elevation={1} className={classes.container}>
+      <List className={classes.list}>
+        <ListSubheader className={classes.header}>{props.name}</ListSubheader>
+        <div className={classes.cards}>
           {props.cards.map((text, idx) => (
-            <Grid item key={idx}>
-              <CardListItem text={text} />
-            </Grid>
+            <CardListItem key={idx} text={text} />
           ))}
-        </Grid>
+        </div>
       </List>
     </Paper>
   );
-};
+});
 
-const Board = props => {
+const Board = withStyles({
+  grid: {
+    margin: 8,
+    wordBreak: "break-word"
+  },
+  gridItem: {
+    width: 300
+  }
+})(props => {
+  const { classes } = props;
   return (
-    <Grid container spacing={16}>
+    <Grid container spacing={16} wrap="nowrap" className={classes.grid}>
       {Object.entries(props.lists).map(([k, v]) => {
         return (
-          <Grid item key={k}>
+          <Grid item key={k} className={classes.gridItem}>
             <CardList name={k} cards={v} />
           </Grid>
         );
       })}
     </Grid>
   );
-};
+});
 
 const App = props => <Board lists={props.lists} />;
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "typeface-roboto";
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "material-ui/CssBaseline";
@@ -6,8 +7,51 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 const lists = {
-  List1: ["card1", "card2", "card3"],
-  List2: ["card1", "card2", "card3"]
+  "This is a long text that will hopefully not overflow": [
+    "card1",
+    "card2",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card1",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card2",
+    "card3"
+  ],
+  List2: [
+    "This is a long text that will hopefully not overflow",
+    "card2",
+    "card3"
+  ]
 };
 
 ReactDOM.render(
