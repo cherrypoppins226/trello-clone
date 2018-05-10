cherrypoppins226/trello-clone

commit a8c9bee932f57dd824e5d11d6d3638e7b14b94d3

Simpler logic in addCard.

src/cardsList.js
@@ -33,26 +33,28 @@ const View = class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: props.cards.map((description, idx) => (
-        <CardsListCard
-          key={idx}
-          description={description}
-          onEditCard={props.onEditCard}
-        />
-      ))
+      cards: props.cards.map((description, idx) =>
+        this.newCard(idx, description)
+      )
     };
+  }
+
+  newCard(idx, description = undefined) {
+    return (
+      <CardsListCard
+        key={idx}
+        description={description}
+        onEditCard={this.props.onEditCard}
+      />
+    );
   }
 
   addCard(e) {
     e.preventDefault();
     this.setState((prevState, props) => {
-      const card = (
-        <CardsListCard
-          key={this.state.cards.length + 1}
-          onEditCard={this.props.onEditCard}
-        />
-      );
-      return { cards: prevState.cards.concat(card) };
+      return {
+        cards: [this.newCard(prevState.cards.length + 1), ...prevState.cards]
+      };
     });
   }
 

commit b46abe8fa055fdc774d2b940063e940971864542

Append (not prepend) new card to cardsList
- Regression introduced in a8c9bee932f57dd824e5d11d6d3638e7b14b94d3
- Add test case to make sure we are appending

src/cardsList.js
@@ -54,7 +54,7 @@ const View = class extends React.Component {
     e.preventDefault();
     this.setState((prevState, props) => ({
       counter: prevState.counter + 1,
-      cards: [this.newCard(prevState.counter + 1), ...prevState.cards]
+      cards: [...prevState.cards, this.newCard(prevState.counter + 1)]
     }));
   }
 

src/cardsListCard.test.js
@@ -22,12 +22,12 @@ it("adds a card", () => {
     />
   );
   const liveList = getByTestId("cards-list");
-  const lengthBefore = liveList.childElementCount;
+  const countBefore = liveList.childElementCount;
+  const lastBefore = liveList.lastElementChild;
   Simulate.click(getByText("Add a card..."));
-  expect(liveList.childElementCount).toBe(lengthBefore + 1);
-  expect(liveList.firstElementChild.tagName).toBe(
-    liveList.lastElementChild.tagName
-  );
+  expect(liveList.childElementCount).toBe(countBefore + 1);
+  expect(liveList.lastElementChild).not.toBe(lastBefore);
+  expect(liveList.lastElementChild.tagName).toBe(lastBefore.tagName);
 });
 
 faker.seed(1);