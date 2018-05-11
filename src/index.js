fa034d2b230d127007916504ea976ab4d0bd1b74
Alejandro Hernandez <alejandro@peggedsoftware.com>
2018-05-11 09:11:19 -0400

    Disable ripple effect in all buttons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "material-ui/CssBaseline";
-import { appData } from "./appData";
-
+import ButtonBase from "material-ui/ButtonBase";
 import "./index.css";
+import { appData } from "./appData";
 import App from "./app";
 import registerServiceWorker from "./registerServiceWorker";
+
+ButtonBase.defaultProps = { ...ButtonBase.defaultProps, disableRipple: true };
 
 ReactDOM.render(
   <React.StrictMode>
