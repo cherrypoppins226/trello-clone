7d68b49a101540a55f242d51080a990da521d429
Alejandro Hernandez <alejandro@peggedsoftware.com>
2018-05-23 15:29:17 -0400

    Re-export main components in index.js files

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Board from "./Board";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import * as Labels from "./labels";
+
+ButtonBase.defaultProps = { ...ButtonBase.defaultProps, disableRipple: true };
+
+const App = ({ lists }) => {
+  return (
+    <>
+      <Board lists={lists} />
+      <div style={{ display: "none" }}>
+        {Object.values(Labels).map((obj, idx) => (
+          <div id={obj.id} key={idx}>
+            {obj.text}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
