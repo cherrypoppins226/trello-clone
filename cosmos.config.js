c57d67895b0a24d7b7c0401aa651355c5df92663
Alejandro Hernandez <alejandro@peggedsoftware.com>
2018-05-24 12:38:58 -0400

    More custom webpack config in cosmos

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,3 +1,7 @@
+const webpack = require("webpack");
+const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+
 const CI = process.env.CI === "1" || process.env.CI === "true";
 
 // TODO: Webpack ignore node_modules folder. We don't need that kind of dynamism
@@ -7,6 +11,16 @@ module.exports = {
   publicPath: "public",
   hot: !CI,
   webpack: (config, { env }) => {
+    config.entry = config.entry.filter(entry => {
+      return CI && !entry.includes("webpackHotDevClient");
+    });
+    config.plugins = config.plugins.filter(plugin => {
+      return (
+        !(CI && plugin instanceof webpack.HotModuleReplacementPlugin) &&
+        !(plugin instanceof WatchMissingNodeModulesPlugin) &&
+        !(plugin instanceof InterpolateHtmlPlugin)
+      );
+    });
     config.devtool = CI ? false : config.devtool;
     return config;
   }
