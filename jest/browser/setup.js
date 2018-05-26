7c6fc96bf68a4ffbb5daa958c48105e1cb2ebff3
Alejandro Hernandez <alejandro@peggedsoftware.com>
2018-05-26 12:17:36 -0400

    Do not rely on CI env variable to start cosmos
    Instead check if the port is available and start it accordingly

--- jest/browser/setup.js
+++ jest/browser/setup.js
@@ -5,46 +5,64 @@ const readline = require("readline");
 const puppeteer = require("puppeteer");
 const { TMPDIR } = require("./common");
 const { exec } = require("child_process");
+const Socket = require("net").Socket;
 
-// TODO: Do not rely on CI to span a new server. Reuse socket if server is running
-// or spin a new one
+const isPortTaken = (port, host = "127.0.0.1", timeout = 1000) => {
+  return new Promise((resolve, reject) => {
+    const socket = new Socket();
 
-const startCosmos = new Promise((resolve, reject) => {
-  if (process.env.CI !== "1" && process.env.CI !== "true") return resolve();
+    socket.setTimeout(timeout);
 
-  const server = exec("yarn cosmos");
-  const rl = readline.createInterface({ input: server.stdout });
-
-  global.__SERVER__ = server;
-
-  server.on("error", error => {
-    global.__SERVER__ = undefined;
-    rl.close();
-    reject(error);
-  });
-
-  // Hacky? Yes. But we need to know when the application has been served.
-  const done = /http:\/\/localhost:\d{4}/;
-
-  rl.on("line", input => {
-    console.log(input);
-    if (!done.test(input)) return;
-    rl.close();
-    resolve();
-  });
-});
-
-module.exports = () =>
-  startCosmos
-    .then(() => puppeteer.launch({}))
-    .then(browser => {
-      global.__BROWSER__ = browser;
-      mkdirp.sync(TMPDIR);
-      fs.writeFileSync(
-        path.join(TMPDIR, "wsEndpoint"),
-        global.__BROWSER__.wsEndpoint()
-      );
-    })
-    .catch(error => {
-      console.error(`Error: ${error}`);
+    socket.on("connect", () => {
+      socket.destroy();
+      resolve(true);
     });
+
+    socket.on("timeout", () => {
+      socket.destroy();
+      reject(Error("Timeout..."));
+    });
+
+    socket.on("error", err => {
+      socket.destroy();
+      if (err.code !== "ECONNREFUSED") return reject(err);
+      resolve(false);
+    });
+
+    socket.connect(port, host);
+  });
+};
+
+const startCosmos = () =>
+  new Promise((resolve, reject) => {
+    const proc = exec("yarn cosmos");
+    const rl = readline.createInterface({ input: proc.stdout });
+
+    proc.on("error", error => {
+      rl.close();
+      reject(error);
+    });
+
+    // Hacky? Yes. But we need to know when the application has been served.
+    const done = /http:\/\/localhost:\d{4}/;
+
+    rl.on("line", input => {
+      console.log(input);
+      if (!done.test(input)) return;
+      rl.close();
+      resolve(proc);
+    });
+  });
+
+module.exports = async () => {
+  const taken = await isPortTaken(8989);
+  if (!taken) {
+    global.__SERVER_PROCESS__ = await startCosmos();
+  }
+  global.__BROWSER__ = await puppeteer.launch({});
+  mkdirp.sync(TMPDIR);
+  fs.writeFileSync(
+    path.join(TMPDIR, "wsEndpoint"),
+    global.__BROWSER__.wsEndpoint()
+  );
+};
--- jest/browser/teardown.js
+++ jest/browser/teardown.js
@@ -2,7 +2,7 @@ const rimraf = require("rimraf");
 const { TMPDIR } = require("./common");
 
 module.exports = async function() {
-  global.__SERVER__ && global.__SERVER__.kill();
+  global.__SERVER_PROCESS__ && global.__SERVER_PROCESS__.kill();
   await global.__BROWSER__.close();
   rimraf.sync(TMPDIR);
 };
